4d4925a1f15e0a2d9a3d2215d8b522aed048a211
jaeho <ovsoce@gmail.com>
2021-12-13 18:01:10 +0900

    feat: JetNews 의 Home 을 가져옴

--- app/src/main/java/io/github/ovso/whitenoise/MainActivity.kt
+++ app/src/main/java/io/github/ovso/whitenoise/MainActivity.kt
@@ -3,12 +3,8 @@ package io.github.ovso.whitenoise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import io.github.ovso.whitenoise.ui.home.Home
 import io.github.ovso.whitenoise.ui.theme.WhiteNoiseTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,43 +12,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WhiteNoiseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    GetList()
+                Surface {
+                    Home()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun GetList() {
-    LazyColumn {
-        // Add a single item
-        item {
-            Text(text = "First item")
-        }
-
-        // Add 5 items
-        items(5) { index ->
-            Text(text = "Item: $index")
-        }
-
-        // Add another single item
-        item {
-            Text(text = "Last item")
-        }
-    }}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WhiteNoiseTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/io/github/ovso/whitenoise/ui/theme/Theme.kt
+++ app/src/main/java/io/github/ovso/whitenoise/ui/theme/Theme.kt
@@ -5,11 +5,31 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
+)
+
+private val DarkColors = darkColors(
+    primary = Red300,
+    primaryVariant = Red700,
+    onPrimary = Color.Black,
+    secondary = Red300,
+    onSecondary = Color.Black,
+    error = Red200
+)
+
+private val LightColors = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
 )
 
 private val LightColorPalette = lightColors(
@@ -39,6 +59,19 @@ fun WhiteNoiseTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
         colors = colors,
         typography = Typography,
         shapes = Shapes,
+        content = content
+    )
+}
+
+@Composable
+fun JetnewsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        typography = JetnewsTypography,
+        shapes = JetnewsShapes,
         content = content
     )
 }
