8026729cb315ba71628a7fec17ea575769323232
jaeho <ovsoce@gmail.com>
2022-01-10 00:57:37 +0900

    chore: wip - Kotlin 으로 마이그레이션
    - spotless 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,41 @@ buildscript {
     }
 }
 
-tasks.register("clean", Delete::class.java) {
-    delete(rootProject.buildDir)
+plugins {
+    id("com.diffplug.gradle.spotless") version "3.27.1"
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.gradle.spotless")
+    val ktlintVer = "0.43.2"
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            ktlint(ktlintVer).userData(
+                mapOf("max_line_length" to "100", "disabled_rules" to "import-ordering")
+            )
+            licenseHeaderFile(project.rootProject.file("copyright.kt"))
+            trimTrailingWhitespace()
+            indentWithSpaces()
+            endWithNewline()
+        }
+        kotlinGradle {
+            // same as kotlin, but for .gradle.kts files (defaults to '*.gradle.kts')
+            target("**/*.gradle.kts")
+            ktlint(ktlintVer)
+            licenseHeaderFile(
+                project.rootProject.file("copyright.kt"),
+                "(plugins |import |include)"
+            )
+            trimTrailingWhitespace()
+            indentWithSpaces()
+            endWithNewline()
+        }
+        format("misc") {
+            target("**/*.md", "**/.gitignore")
+            trimTrailingWhitespace()
+            indentWithSpaces()
+            endWithNewline()
+        }
+    }
 }
