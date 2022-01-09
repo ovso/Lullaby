925429d6d747858ea505a5175dc14703053e4898
jaeho <ovsoce@gmail.com>
2022-01-09 16:33:04 +0900

    chore: wip - Kotlin 으로 마이그레이션

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,9 +4,12 @@ import org.gradle.kotlin.dsl.project
 
 object Dependencies {
 
+    const val APPLICATION_ID = "io.github.ovso.lullaby"
     const val COMPILE_SDK = 31
     const val MIN_SDK = 26
     const val TARGET_SDK = 31
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
 
     private const val implementation = "implementation"
     private const val testImplementation = "testImplementation"
