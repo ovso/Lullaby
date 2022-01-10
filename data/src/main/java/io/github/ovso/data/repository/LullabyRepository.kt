c58131b36872d3a9a4dc5fa77c91fb3538391cbb
jaeho <ovsoce@gmail.com>
2022-01-11 07:13:21 +0900

    feat: data 에 인터페이스, UseCase 생성

---
+++ data/src/main/java/io/github/ovso/data/repository/LullabyRepository.kt
@@ -0,0 +1,4 @@
+package io.github.ovso.data.repository
+
+interface LullabyRepository {
+}
---
+++ data/src/main/java/io/github/ovso/data/usecase/LullabyUseCase.kt
@@ -0,0 +1,8 @@
+package io.github.ovso.data.usecase
+
+import io.github.ovso.data.repository.LullabyRepository
+
+class LullabyUseCase(
+  private val repository: LullabyRepository
+) {
+}
