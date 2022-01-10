a06ac4e9600fde7af8892911ac7f40e3f7f20859
jaeho <ovsoce@gmail.com>
2022-01-11 07:22:31 +0900

    feat: domain 을 안드로이드 라이브러리에서 -> 자바 라이브러리로 변경

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,55 +1,12 @@
-import Dependencies.implementationTest
-
-/*
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 plugins {
-  id("com.android.library")
-  id("kotlin-android")
+  id("java-library")
+  id("kotlin")
 }
 
-android {
-  compileSdk = Dependencies.COMPILE_SDK
-
-  defaultConfig {
-    minSdk = Dependencies.MIN_SDK
-    targetSdk = Dependencies.TARGET_SDK
-    consumerProguardFiles("consumer-rules.pro")
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
 }
 
 dependencies {
-  implementation(Dependencies.AndroidX.CORE)
-  implementation(Dependencies.AndroidX.APPCOMPAT)
-  implementation(Dependencies.Google.MATERIAL)
-  implementationTest()
 }
