756501b6962bd3a72fa486f8306b88df0cbdc97c
jaeho <ovsoce@gmail.com>
2022-01-10 12:07:37 +0900

    chore: 종속성 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,118 +1,86 @@
-/*
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import Dependencies.implementationAccompanist
+import Dependencies.implementationAndroidX
+import Dependencies.implementationCompose
+import Dependencies.implementationGoogle
+import Dependencies.implementationHilt
+import Dependencies.implementationKotlin
+import Dependencies.implementationLifecycle
+import Dependencies.implementationTest
+
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("kotlin-kapt")
-    id("kotlinx-serialization")
-    id("kotlin-parcelize")
+  id("com.android.application")
+  id("kotlin-android")
+  id("kotlin-kapt")
+  id("kotlinx-serialization")
+  id("kotlin-parcelize")
 }
 
 android {
-    compileSdk = Dependencies.COMPILE_SDK
+  compileSdk = Dependencies.COMPILE_SDK
 
-    defaultConfig {
-        applicationId = Dependencies.APPLICATION_ID
-        minSdk = Dependencies.MIN_SDK
-        targetSdk = Dependencies.TARGET_SDK
-        versionCode = Dependencies.VERSION_CODE
-        versionName = Dependencies.VERSION_NAME
+  defaultConfig {
+    applicationId = Dependencies.APPLICATION_ID
+    minSdk = Dependencies.MIN_SDK
+    targetSdk = Dependencies.TARGET_SDK
+    versionCode = Dependencies.VERSION_CODE
+    versionName = Dependencies.VERSION_NAME
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables {
+      useSupportLibrary = true
+    }
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      isDebuggable = false
+      proguardFile(getDefaultProguardFile("proguard-android.txt"))
+      // global proguard settings
+      proguardFile(file("proguard-rules.pro"))
+      // library proguard settings
+      val files = rootProject.file("proguard")
+        .listFiles()
+        ?.filter { it.name.startsWith("proguard") }
+        ?.toTypedArray() ?: arrayOf()
+
+      proguardFiles(*files)
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            isDebuggable = false
-            proguardFile(getDefaultProguardFile("proguard-android.txt"))
-            // global proguard settings
-            proguardFile(file("proguard-rules.pro"))
-            // library proguard settings
-            val files = rootProject.file("proguard")
-                .listFiles()
-                ?.filter { it.name.startsWith("proguard") }
-                ?.toTypedArray() ?: arrayOf()
-
-            proguardFiles(*files)
-        }
-
-        debug {
-            isDebuggable = true
-        }
+    debug {
+      isDebuggable = true
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+  kotlinOptions {
+    jvmTarget = "11"
+  }
+  buildFeatures {
+    compose = true
+    viewBinding = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.0.5"
+  }
+  packagingOptions {
+    resources {
+      with(excludes) {
+        add("/META-INF/{AL2.0,LGPL2.1}")
+      }
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-        viewBinding = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.5"
-    }
-    packagingOptions {
-        resources {
-            with(excludes) {
-                add("/META-INF/{AL2.0,LGPL2.1}")
-            }
-        }
-    }
+  }
 }
 
 dependencies {
-
-    val compose_version = "1.0.5"
-    val accompanist_version = "0.21.3-beta"
-
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:$compose_version")
-    implementation("androidx.compose.material:material:$compose_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
-    implementation("androidx.compose.material:material-icons-extended:$compose_version")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
-    implementation("androidx.activity:activity-compose:1.4.0")
-
-    implementation("com.google.accompanist:accompanist-swiperefresh:$accompanist_version")
-    implementation("com.google.accompanist:accompanist-insets:$accompanist_version")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanist_version")
-
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
-
-    implementation("androidx.navigation:navigation-compose:2.4.0-rc01")
-
-    implementation("androidx.window:window:1.0.0-rc01")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
-    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
+  implementationAndroidX()
+  implementationGoogle()
+  implementationCompose()
+  implementationLifecycle()
+  implementationAccompanist()
+  implementationKotlin()
+  implementationHilt()
+  implementationTest()
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,118 +1,152 @@
+@file:Suppress("SpellCheckingInspection")
+
 import org.gradle.kotlin.dsl.DependencyHandlerScope
-import org.gradle.kotlin.dsl.exclude
-import org.gradle.kotlin.dsl.project
 
 object Dependencies {
 
-    const val APPLICATION_ID = "io.github.ovso.lullaby"
-    const val COMPILE_SDK = 31
-    const val MIN_SDK = 26
-    const val TARGET_SDK = 31
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0"
+  const val APPLICATION_ID = "io.github.ovso.lullaby"
+  const val COMPILE_SDK = 31
+  const val MIN_SDK = 26
+  const val TARGET_SDK = 31
+  const val VERSION_CODE = 1
+  const val VERSION_NAME = "1.0"
 
-    private const val implementation = "implementation"
-    private const val testImplementation = "testImplementation"
-    private const val androidTestImplementation = "androidTestImplementation"
+  private const val implementation = "implementation"
+  private const val testImplementation = "testImplementation"
+  private const val androidTestImplementation = "androidTestImplementation"
+  private const val debugImplementation = "debugImplementation"
 
-    private const val kapt = "kapt"
-    private const val kaptTest = "kaptTest"
-    private const val kaptAndroidTest = "kaptAndroidTest"
-
-
-
-    object Kotlin{
-        private const val VERSION = "1.5.31"
-        const val STD_LIB = "org.jetbrains.kotlin:kotlin-stdlib:$VERSION"
-        const val TEST = "org.jetbrains.kotlin:kotlin-test-junit:$VERSION"
-
-        const val COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VERSION"
-        const val COROUTINE_TEST = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1"
-        const val COROUTINE_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VERSION"
-    }
-    object Google{
-        const val MATERIAL = "com.google.android.material:material:1.4.0"
-    }
-
-    object AndroidX {
-        const val APPCOMPAT = "androidx.appcompat:appcompat:1.3.1"
-        const val RECYCLER_VIEW = "androidx.recyclerview:recyclerview:1.2.0"
-        const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.0"
-        const val ACTIVITY = "androidx.activity:activity-ktx:1.2.2"
-        const val FRAGMENT = "androidx.fragment:fragment-ktx:1.3.2"
-        const val CORE = "androidx.core:core-ktx:1.6.0"
-
-        private const val VERSION_PAGING = "3.0.0"
-        const val PAGING_RUNTIME = "androidx.paging:paging-runtime-ktx:$VERSION_PAGING"
-        const val PAGING_COMMON = "androidx.paging:paging-common:$VERSION_PAGING"
-
-    }
-
-    fun DependencyHandlerScope.applyAndroidX(){
-        implementation(AndroidX.APPCOMPAT)
-        implementation(AndroidX.CORE)
-        implementation(AndroidX.ACTIVITY)
-        implementation(AndroidX.FRAGMENT)
-        implementation(AndroidX.RECYCLER_VIEW)
-        implementation(AndroidX.CONSTRAINT_LAYOUT)
-        implementation(AndroidX.PAGING_RUNTIME)
-    }
-
-    object Retrofit2{
-        private const val VERSION = "2.9.0"
-        const val CORE = "com.squareup.retrofit2:retrofit:${VERSION}"
-        const val MOSHI = "com.squareup.retrofit2:converter-moshi:${VERSION}"
-        const val SCALARS = "com.squareup.retrofit2:converter-scalars:${VERSION}"
-    }
-
-    fun DependencyHandlerScope.applyRetrofit2(){
-        implementation(Retrofit2.CORE)
-        implementation(Retrofit2.MOSHI)
-        implementation(Retrofit2.SCALARS)
-    }
+  private const val kapt = "kapt"
+  private const val kaptTest = "kaptTest"
+  private const val kaptAndroidTest = "kaptAndroidTest"
 
 
-//    object Lifecycle{
-//        const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
-//        const val VIEWMODEL_SAVEDSTATE = "androidx.lifecycle:lifecycle-viewmodel-savedstate:${Versions.lifecycle}"
-//        const val RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}"
-//        const val EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleExt}"
-//        const val COMMON_JAVA8 = "androidx.lifecycle:lifecycle-common-java8:${Versions.lifecycle}"
-//    }
-//
-//    fun DependencyHandlerScope.applyLifecycle(){
-//        implementation(Lifecycle.VIEWMODEL)
-//        implementation(Lifecycle.VIEWMODEL_SAVEDSTATE)
-//        implementation(Lifecycle.COMMON_JAVA8)
-//        implementation(Lifecycle.EXTENSIONS)
-//        implementation(Lifecycle.RUNTIME)
-//    }
+  object Kotlin {
+    private const val VERSION = "1.6.0"
+    const val STD_LIB = "org.jetbrains.kotlin:kotlin-stdlib:$VERSION"
+    const val TEST_JUNIT = "org.jetbrains.kotlin:kotlin-test-junit:$VERSION"
 
-//    object Navigation{
-//        const val FRAGMENT = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
-//        const val UI = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
-//        const val DYNAMIC_FEATURE_FRAGMENT = "androidx.navigation:navigation-dynamic-features-fragment:${Versions.navigation}"
-//    }
-//
-//    fun DependencyHandlerScope.applyNavigation(){
-//        implementation(Navigation.FRAGMENT)
-//        implementation(Navigation.UI)
-//        implementation(Navigation.DYNAMIC_FEATURE_FRAGMENT)
-//    }
+    const val COROUTINE_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VERSION"
+    const val COROUTINE_TEST = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0"
+    const val COROUTINE_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VERSION"
 
-    object Glide{
-        const val VERSION = "4.12.0"
-        const val CORE = "com.github.bumptech.glide:glide:$VERSION"
-        const val COMPILER = "com.github.bumptech.glide:compiler:$VERSION"
-        const val OKHTTP3_INTEGRATION = "com.github.bumptech.glide:okhttp3-integration:$VERSION"
-        const val TRANSFORMATIONS = "jp.wasabeef:glide-transformations:4.3.0"
-    }
+    const val SERIALIZATION = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2"
+  }
 
-    fun DependencyHandlerScope.applyGlide(){
-        implementation(Glide.CORE)
-        kapt(Glide.COMPILER)
-        implementation(Glide.OKHTTP3_INTEGRATION)
-    }
+  fun DependencyHandlerScope.implementationKotlin() {
+    implementation(Kotlin.STD_LIB)
+    implementation(Kotlin.TEST_JUNIT)
+    implementation(Kotlin.COROUTINE_ANDROID)
+    implementation(Kotlin.COROUTINE_CORE)
+    implementation(Kotlin.COROUTINE_TEST)
+    implementation(Kotlin.SERIALIZATION)
+  }
+
+  object Google {
+    const val MATERIAL = "com.google.android.material:material:1.4.0"
+  }
+
+  fun DependencyHandlerScope.implementationGoogle() {
+    implementation(Google.MATERIAL)
+  }
+
+  object AndroidX {
+    const val CORE = "androidx.core:core-ktx:1.7.0"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:1.4.0"
+    const val ACTIVITY_COMPOSE = "androidx.activity:activity-compose:1.4.0"
+    const val NAVIGATION_COMPOSE = "androidx.navigation:navigation-compose:2.4.0-rc01"
+    const val WINDOW = "androidx.window:window:1.0.0-rc01"
+    const val TEST_EXT_JUNIT = "androidx.test.ext:junit:1.1.3"
+    const val TEST_ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.4.0"
+  }
+
+  fun DependencyHandlerScope.implementationAndroidX() {
+    implementation(AndroidX.APPCOMPAT)
+    implementation(AndroidX.CORE)
+    implementation(AndroidX.ACTIVITY_COMPOSE)
+    implementation(AndroidX.NAVIGATION_COMPOSE)
+    implementation(AndroidX.WINDOW)
+    androidTestImplementation(AndroidX.TEST_EXT_JUNIT)
+    androidTestImplementation(AndroidX.TEST_ESPRESSO_CORE)
+  }
+
+  object Retrofit2 {
+    private const val VERSION = "2.9.0"
+    const val CORE = "com.squareup.retrofit2:retrofit:${VERSION}"
+    const val MOSHI = "com.squareup.retrofit2:converter-moshi:${VERSION}"
+    const val SCALARS = "com.squareup.retrofit2:converter-scalars:${VERSION}"
+  }
+
+  fun DependencyHandlerScope.implementationRetrofit2() {
+    implementation(Retrofit2.CORE)
+    implementation(Retrofit2.MOSHI)
+    implementation(Retrofit2.SCALARS)
+  }
+
+  object Compose {
+    private const val VERSION = "1.0.5"
+
+    const val UI = "androidx.compose.ui:ui:$VERSION"
+    const val PREVIEW = "androidx.compose.ui:ui-tooling-preview:$VERSION"
+    const val MATERIAL = "androidx.compose.material:material:$VERSION"
+    const val MATERIAL_ICONS = "androidx.compose.material:material-icons-extended:$VERSION"
+    const val UI_TEST_JUNIT4 = "androidx.compose.ui:ui-test-junit4:$VERSION"
+    const val UI_TOOLING = "androidx.compose.ui:ui-tooling:$VERSION"
+  }
+
+  fun DependencyHandlerScope.implementationCompose() {
+    implementation(Compose.UI)
+    implementation(Compose.PREVIEW)
+    implementation(Compose.MATERIAL)
+    implementation(Compose.MATERIAL_ICONS)
+    androidTestImplementation(Compose.UI_TEST_JUNIT4)
+    debugImplementation(Compose.UI_TOOLING)
+  }
+
+  object Lifecycle {
+    private const val VERSION = "2.4.0"
+    const val VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:$VERSION"
+    const val VIEWMODEL_COMPOSE = "androidx.lifecycle:lifecycle-viewmodel-compose:$VERSION"
+    const val VIEWMODEL_SAVEDSTATE = "androidx.lifecycle:lifecycle-viewmodel-savedstate:$VERSION"
+    const val RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:$VERSION"
+    const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata-ktx:$VERSION"
+  }
+
+  fun DependencyHandlerScope.implementationLifecycle() {
+    implementation(Lifecycle.LIVEDATA)
+    implementation(Lifecycle.RUNTIME)
+    implementation(Lifecycle.VIEWMODEL_COMPOSE)
+    implementation(Lifecycle.VIEWMODEL_KTX)
+    implementation(Lifecycle.VIEWMODEL_SAVEDSTATE)
+  }
+
+  object Accompanist {
+    private const val VERSION = "0.22.0-rc"
+    const val SWIPEREFRESH = "com.google.accompanist:accompanist-swiperefresh:$VERSION"
+    const val INSETS = "com.google.accompanist:accompanist-insets:$VERSION"
+    const val SYSTEMUICONTROLLER = "com.google.accompanist:accompanist-systemuicontroller:$VERSION"
+  }
+
+  fun DependencyHandlerScope.implementationAccompanist() {
+    implementation(Accompanist.SWIPEREFRESH)
+    implementation(Accompanist.INSETS)
+    implementation(Accompanist.SYSTEMUICONTROLLER)
+  }
+
+
+  object Glide {
+    private const val VERSION = "4.12.0"
+    const val CORE = "com.github.bumptech.glide:glide:$VERSION"
+    const val COMPILER = "com.github.bumptech.glide:compiler:$VERSION"
+    const val OKHTTP3_INTEGRATION = "com.github.bumptech.glide:okhttp3-integration:$VERSION"
+    const val TRANSFORMATIONS = "jp.wasabeef:glide-transformations:4.3.0"
+  }
+
+  fun DependencyHandlerScope.implementationGlide() {
+    implementation(Glide.CORE)
+    kapt(Glide.COMPILER)
+    implementation(Glide.OKHTTP3_INTEGRATION)
+  }
 
 //    object JakeWharton{
 //        const val THREE_TEN_ABP = "com.jakewharton.threetenabp:threetenabp:${Versions.threetenabp}"
@@ -121,43 +155,41 @@ object Dependencies {
 //        const val RXBINDING = "com.jakewharton.rxbinding2:rxbinding:${Versions.rxbinding}"
 //    }
 
-    object Test{
-        const val JUNIT = "junit:junit:4.13.2"
-        const val ANDROID_EXT_JUNIT = "androidx.test.ext:junit:1.1.3"
-        const val ANDROID_ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.4.0"
-    }
+  object Test {
+    const val JUNIT = "junit:junit:4.13.2"
+    const val ANDROID_EXT_JUNIT = "androidx.test.ext:junit:1.1.3"
+    const val ANDROID_ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.4.0"
+  }
 
-    fun DependencyHandlerScope.applyTest(){
-        Dependencies.testImplementation(Test.JUNIT)
-        androidTestImplementation(Test.ANDROID_EXT_JUNIT)
-        androidTestImplementation(Test.ANDROID_ESPRESSO_CORE)
-    }
+  fun DependencyHandlerScope.implementationTest() {
+    Dependencies.testImplementation(Test.JUNIT)
+  }
 
-    object Hilt{
-        const val VERSION = "2.38.1"
+  object Hilt {
+    private const val VERSION = "2.40.5"
 
-        const val CORE = "com.google.dagger:hilt-android:$VERSION"
-        const val COMPILER = "com.google.dagger:hilt-compiler:$VERSION"
+    const val CORE = "com.google.dagger:hilt-android:$VERSION"
+    const val COMPILER = "com.google.dagger:hilt-compiler:$VERSION"
 
-        // For instrumentation tests
-        const val ANDROID_TESTING = "com.google.dagger:hilt-android-testing:$VERSION"
-        const val ANDROID_TESTING_COMPILER = "com.google.dagger:hilt-compiler:$VERSION"
+    // For instrumentation tests
+    const val ANDROID_TESTING = "com.google.dagger:hilt-android-testing:$VERSION"
+    const val ANDROID_TESTING_COMPILER = "com.google.dagger:hilt-compiler:$VERSION"
 
-        // For local unit tests
-        const val LOCAL_TESTING = "com.google.dagger:hilt-android-testing:$VERSION"
-        const val LOCAL_TESTING_COMPILER = "com.google.dagger:hilt-compiler:$VERSION"
-    }
+    // For local unit tests
+    const val LOCAL_TESTING = "com.google.dagger:hilt-android-testing:$VERSION"
+    const val LOCAL_TESTING_COMPILER = "com.google.dagger:hilt-compiler:$VERSION"
+  }
 
-    fun DependencyHandlerScope.applyHilt(){
-        implementation(Hilt.CORE)
-        kapt(Hilt.COMPILER)
+  fun DependencyHandlerScope.implementationHilt() {
+    implementation(Hilt.CORE)
+    kapt(Hilt.COMPILER)
 
-        kaptTest(Hilt.LOCAL_TESTING_COMPILER)
-        testImplementation(Hilt.LOCAL_TESTING)
+    kaptTest(Hilt.LOCAL_TESTING_COMPILER)
+    testImplementation(Hilt.LOCAL_TESTING)
 
-        androidTestImplementation(Hilt.ANDROID_TESTING)
-        kaptAndroidTest(Hilt.ANDROID_TESTING_COMPILER)
-    }
+    androidTestImplementation(Hilt.ANDROID_TESTING)
+    kaptAndroidTest(Hilt.ANDROID_TESTING_COMPILER)
+  }
 
 
 }
