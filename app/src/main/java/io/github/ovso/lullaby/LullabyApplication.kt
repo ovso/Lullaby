108ad49c8f678fd0a931f4a2ffe2b7572d437ceb
jaeho <ovsoce@gmail.com>
2022-01-13 18:02:55 +0900

    feat: Hilt 어노테이션 제거

--- app/src/main/java/io/github/ovso/lullaby/LullabyApplication.kt
+++ app/src/main/java/io/github/ovso/lullaby/LullabyApplication.kt
@@ -1,15 +1,12 @@
 package io.github.ovso.lullaby
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
 import io.github.ovso.lullaby.data.AppContainer
 import io.github.ovso.lullaby.data.AppContainerImpl
 import io.github.ovso.lullaby.player.LullabyPlayer
 import io.github.ovso.lullaby.player.LullabyPlayerImpl
 
-@HiltAndroidApp
 class LullabyApplication : Application() {
-  // AppContainer instance used by the rest of classes to obtain dependencies
   lateinit var container: AppContainer
   lateinit var player: LullabyPlayer
 
