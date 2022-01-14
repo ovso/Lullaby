a32779708802a6a238634c55331198ca6c53c28f
jaeho <ovsoce@gmail.com>
2022-01-14 17:40:04 +0900

    feat: WAKE_LOCK 진행중

--- app/src/main/java/io/github/ovso/lullaby/player/LullabyPlayerImpl.kt
+++ app/src/main/java/io/github/ovso/lullaby/player/LullabyPlayerImpl.kt
@@ -2,6 +2,7 @@ package io.github.ovso.lullaby.player
 
 import android.content.Context
 import android.media.MediaPlayer
+import android.os.PowerManager
 
 interface LullabyPlayer {
   fun play(resName: String)
@@ -15,7 +16,10 @@ class LullabyPlayerImpl(
   private var player: MediaPlayer? = null
   override fun play(resName: String) {
     val resId = context.resources.getIdentifier(resName, "raw", context.packageName)
-    player = MediaPlayer.create(context, resId).apply { isLooping = true }
+    player = MediaPlayer.create(context, resId).apply {
+      isLooping = true
+      setWakeMode(context, PowerManager.PARTIAL_WAKE_LOCK)
+    }
     player?.start()
   }
 
--- app/src/main/java/io/github/ovso/lullaby/ui/MainActivity.kt
+++ app/src/main/java/io/github/ovso/lullaby/ui/MainActivity.kt
@@ -1,35 +1,27 @@
-/*
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.github.ovso.lullaby.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import io.github.ovso.lullaby.LullabyApplication
+import io.github.ovso.lullaby.service.LullabyService
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        val app = application as LullabyApplication
-        setContent {
-            LullabyApp(app.container, this)
-        }
+    val app = application as LullabyApplication
+    setContent {
+      LullabyApp(app.container, this)
     }
+  }
+
+  override fun onBackPressed() {
+    super.onBackPressed()
+    applicationContext.stopService(Intent(this, LullabyService::class.java))
+  }
 }
