d29a9c92ab99b3b7bd70cff9b02fa66c9b9e536e
jaeho <ovsoce@gmail.com>
2022-01-25 10:23:05 +0900

    feat: onBind 에 null 반환함
    - UI 와 통신하지 않는다.

--- app/src/main/java/io/github/ovso/lullaby/service/LullabyService.kt
+++ app/src/main/java/io/github/ovso/lullaby/service/LullabyService.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.github.ovso.lullaby.service
 
 import android.app.NotificationChannel
@@ -21,7 +6,6 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import android.os.Binder
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import io.github.ovso.lullaby.player.LullabyPlayerImpl
@@ -31,11 +15,7 @@ const val NOTIFICATION_ID = 10
 const val CHANNEL_ID = "primary_notification_channel"
 
 class LullabyService : Service() {
-  private val binder = AudioServiceBinder()
-
   private val mediaPlayer by lazy { LullabyPlayerImpl(applicationContext) }
-
-  private class AudioServiceBinder : Binder()
 
   override fun onCreate() {
     super.onCreate()
@@ -52,8 +32,8 @@ class LullabyService : Service() {
     return super.onStartCommand(intent, flags, startId)
   }
 
-  override fun onBind(intent: Intent?): IBinder {
-    return binder
+  override fun onBind(intent: Intent?): IBinder? {
+    return null
   }
 
   override fun onDestroy() {
