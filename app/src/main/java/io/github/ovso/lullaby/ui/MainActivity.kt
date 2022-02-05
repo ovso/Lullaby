a252396ec12633f78d1ff049fb39653c7e3153b0
jaeho <ovsoce@gmail.com>
2022-02-06 08:06:05 +0900

    feat: 테스트 다이얼로그 제거

--- app/src/main/java/io/github/ovso/lullaby/ui/MainActivity.kt
+++ app/src/main/java/io/github/ovso/lullaby/ui/MainActivity.kt
@@ -3,11 +3,9 @@ package io.github.ovso.lullaby.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import io.github.ovso.lullaby.LullabyApplication
-import io.github.ovso.lullaby.R
 import io.github.ovso.lullaby.exts.showOpeningAds
 import io.github.ovso.lullaby.service.LullabyService
 
@@ -21,8 +19,6 @@ class MainActivity : AppCompatActivity() {
     setContent {
       LullabyApp(app.container, this)
     }
-
-    AlertDialog.Builder(this).setMessage(getString(R.string.ads_unit_id_open)).show()
   }
 
   override fun onBackPressed() {
