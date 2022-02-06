614d74f85aa598a17eda96d9bccecd22d2645dec
jaeho <ovsoce@gmail.com>
2022-02-06 16:42:39 +0900

    chore: 2 / 1.0.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,8 +8,8 @@ object Dependencies {
   const val COMPILE_SDK = 31
   const val MIN_SDK = 26
   const val TARGET_SDK = 31
-  const val VERSION_CODE = 1
-  const val VERSION_NAME = "1.0.0"
+  const val VERSION_CODE = 2
+  const val VERSION_NAME = "1.0.1"
 
   private const val implementation = "implementation"
   private const val testImplementation = "testImplementation"
