db48627533a0003f651c3d71193783fcc5d7e900
jaeho <ovsoce@gmail.com>
2022-03-06 20:09:39 +0900

    feat: 디버그 모드일 때 테스트단말 등록

--- app/src/main/java/io/github/ovso/lullaby/LullabyApplication.kt
+++ app/src/main/java/io/github/ovso/lullaby/LullabyApplication.kt
@@ -2,15 +2,25 @@ package io.github.ovso.lullaby
 
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
+import io.github.ovso.winfo.DEBUG
 
 @HiltAndroidApp
 class LullabyApplication : Application() {
   override fun onCreate() {
     super.onCreate()
+
+    if (DEBUG) {
+      val testDeviceIds = listOf("1D30C2DF60EE05B068D4D17AC4072D7C")
+      val configuration = RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build()
+      MobileAds.setRequestConfiguration(configuration)
+
+      Logger.addLogAdapter(AndroidLogAdapter())
+    }
+
     MobileAds.initialize(this) {}
-    Logger.addLogAdapter(AndroidLogAdapter())
   }
 }
