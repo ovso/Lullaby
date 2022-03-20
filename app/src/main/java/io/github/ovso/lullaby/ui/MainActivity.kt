619caacde5cb72cb591c1a2509a2f5960d6c73ff
jaeho <ovsoce@gmail.com>
2022-03-21 03:20:19 +0900

    feat: 광고 시점 변경, 오프닝광고로 변경

--- app/src/main/java/io/github/ovso/lullaby/ui/MainActivity.kt
+++ app/src/main/java/io/github/ovso/lullaby/ui/MainActivity.kt
@@ -5,9 +5,10 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import com.orhanobut.logger.Logger
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.FullScreenContentCallback
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.ovso.lullaby.exts.showInterstitialAd
+import io.github.ovso.lullaby.exts.showOpeningAds
 import io.github.ovso.lullaby.service.LullabyService
 
 @AndroidEntryPoint
@@ -15,14 +16,24 @@ class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    showInterstitialAd({
-      Logger.d("ad error: $it")
-    }) {
-      it.show(this)
-    }
     WindowCompat.setDecorFitsSystemWindows(window, false)
+    showOpeningAds(object : FullScreenContentCallback() {
+      override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+        super.onAdFailedToShowFullScreenContent(p0)
+        showContent()
+      }
+
+      override fun onAdDismissedFullScreenContent() {
+        super.onAdDismissedFullScreenContent()
+        showContent()
+      }
+
+    })
+  }
+
+  private fun showContent() {
     setContent {
-      LullabyApp(context = this)
+      LullabyApp(context = this@MainActivity)
     }
   }
 
