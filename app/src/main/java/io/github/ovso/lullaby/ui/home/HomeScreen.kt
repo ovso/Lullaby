7dafb745842ab366929419307504b5b200ff4aa3
jaeho <ovsoce@gmail.com>
2022-04-07 23:28:26 +0900

    style: 린트에러 해결
    IDE 내에서 해결 해야 할 필요가 있다

--- app/src/main/java/io/github/ovso/lullaby/ui/home/HomeScreen.kt
+++ app/src/main/java/io/github/ovso/lullaby/ui/home/HomeScreen.kt
@@ -1,22 +1,13 @@
+@file:Suppress("FunctionName")
+
 package io.github.ovso.lullaby.ui.home
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.Divider
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
