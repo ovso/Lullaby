8c94efd67bfdde0dd4dedc63c184dc9bea5d7cdb
jaeho <ovsoce@gmail.com>
2022-04-07 23:25:22 +0900

    style: 코드정렬

--- app/src/main/java/io/github/ovso/lullaby/utils/AppOpenManager.kt
+++ app/src/main/java/io/github/ovso/lullaby/utils/AppOpenManager.kt
@@ -6,38 +6,34 @@ import com.google.android.gms.ads.appopen.AppOpenAd
 import io.github.ovso.lullaby.R
 
 class AppOpenManager(app: Application) {
-    private var appOpenAd: AppOpenAd? = null
-    lateinit var loadCallback: AppOpenAd.AppOpenAdLoadCallback
+  private var appOpenAd: AppOpenAd? = null
+  lateinit var loadCallback: AppOpenAd.AppOpenAdLoadCallback
 
-    private val application: Application = app
+  private val application: Application = app
 
-    /** Request an ad  */
-    fun fetchAd() {
-        // Have unused ad, no need to fetch another.
-        if (isAdAvailable) {
-            return
-        }
-        val request: AdRequest = adRequest
-        AppOpenAd.load(
-            application,
-            application.getString(R.string.ads_unit_id_open),
-            request,
-            AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT,
-            loadCallback
-        )
-
+  /** Request an ad  */
+  fun fetchAd() {
+    // Have unused ad, no need to fetch another.
+    if (isAdAvailable) {
+      return
     }
+    val request: AdRequest = adRequest
+    AppOpenAd.load(
+      application,
+      application.getString(R.string.ads_unit_id_open),
+      request,
+      AppOpenAd.APP_OPEN_AD_ORIENTATION_PORTRAIT,
+      loadCallback
+    )
 
-    /** Creates and returns ad request.  */
-    private val adRequest: AdRequest
-        get() = AdRequest.Builder().build()
+  }
 
-    /** Utility method that checks if ad exists and can be shown.  */
-    private val isAdAvailable: Boolean
-        get() = appOpenAd != null
+  /** Creates and returns ad request.  */
+  private val adRequest: AdRequest
+    get() = AdRequest.Builder().build()
 
-    companion object {
-        private const val LOG_TAG = "AppOpenManager"
-    }
+  /** Utility method that checks if ad exists and can be shown.  */
+  private val isAdAvailable: Boolean
+    get() = appOpenAd != null
 
 }
